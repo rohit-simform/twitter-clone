bc3a33e5934485c6e9b6ba3c15d6d26a10948b86
srmarohit <srmarohit@gmail.com>
2024-10-19 01:05:52 +0530

    implement follower and following api

--- app/apis/v1/endpoints.py
+++ app/apis/v1/endpoints.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 from app.apis.v1.auth import router as auth
+from app.apis.v1.users import router as users
 from app.apis.v1.tweets import router as tweets
 
 router = APIRouter()
 
 api_v1_router = APIRouter()
 api_v1_router.include_router(auth, prefix="/auth", tags=["auth"])
+api_v1_router.include_router(users, prefix="/user", tags=["user"])
 api_v1_router.include_router(tweets, prefix="/tweets", tags=["tweets"])
--- app/apis/v1/users.py
+++ app/apis/v1/users.py
@@ -1,16 +1,88 @@
 
 from fastapi import HTTPException, status
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
+from app.db.mongo_conn import userCollection, userCircleCollection
+
+from app.utils.utils import verify_token
 router = APIRouter()
 
 # follow user api
 
 
-@router.get("/follow/:follow_user_id")
-async def follow_user():
+@router.get("/follow/{follow_user_id}")
+async def follow_user(follow_user_id: str, request: Request):
     # fetch userId from jwt
-    # check follow_user_id is already exists or not in db
-    # if exists then remove [from userId and follow_user_id doc otherwise add.
-    # for adding --
-    # add follow_user_id into following field as respect to userId
-    # similary add userId into followers field as respect to follow_userId
+    access_token = request.headers.get('Authorization')
+    print(access_token)
+    access_token = access_token.split(" ")
+
+    # HTTPException Error response format
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    print(access_token[1])
+
+    token_data = verify_token(access_token[1], credentials_exception)
+
+    if token_data is None:
+        return {
+            "success": False,
+            "msg": "Token is invalid or expired."
+        }
+
+    userId = token_data.userId
+
+    userCircle = await userCircleCollection.find_one({"userId": userId})
+    followUserCircle = await userCircleCollection.find_one({"userId": follow_user_id})
+
+    print(userCircle)
+    print(followUserCircle)
+    if userCircle is None or followUserCircle is None:
+        return {
+            "success": False,
+            "msg": "Unknown or missing Ids."
+        }
+
+    isAlreadyFollowedUser = follow_user_id in userCircle["following"]
+
+    print(isAlreadyFollowedUser)
+    if isAlreadyFollowedUser:
+        # already following user then unfollow here
+        userCircle["following"].remove(follow_user_id)
+        followUserCircle["followers"].remove(userId)
+    else:
+        # add follow_user_id into following list
+        userCircle["following"].append(follow_user_id)
+        followUserCircle["followers"].append(userId)
+
+    print(userCircle)
+    print(follow_user_id)
+
+    # update following
+    result = await userCircleCollection.update_one(
+        {"userId": userId},  # Filter to match the document
+        {"$set": {"following": userCircle["following"]}}  # Update operation
+    )
+
+    if result is None:
+        return {
+            "success": False,
+            "msg": "Something went wrong."
+        }
+
+    # once following updation is done successfully then
+    # update followers list of follow user cricle
+    await userCircleCollection.update_one(
+        {"userId": follow_user_id},  # Filter to match the document
+        # Update operation
+        {"$set": {"followers": followUserCircle["followers"]}}
+    )
+
+    print(result)
+    return {
+        "success": True,
+        "msg": "You are now following this user."
+    }
